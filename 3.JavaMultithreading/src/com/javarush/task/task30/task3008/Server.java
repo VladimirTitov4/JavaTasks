bc7798e18f111255c3a93e0991307ae195ba271f
VladimirTitov4 <vtitov4@yandex.ru>
2020-07-12 21:13:12 +0400

    Creating online Chat

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,92 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Сервер запущен");
+            while (true) {
+                Handler handler = new Handler(serverSocket.accept());
+                handler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME) continue;
+                if (message.getData() != null && !message.getData().equals("") && message.getType() == MessageType.USER_NAME) {
+                    if (!connectionMap.containsKey(message.getData())) {
+                        connectionMap.put(message.getData(), connection);
+                        connection.send(new Message(MessageType.NAME_ACCEPTED));
+                        ConsoleHelper.writeMessage("Принято");
+                        return message.getData();
+                    }
+                }
+            }
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException {
+            for (String s : connectionMap.keySet()) {
+                if (!userName.equals(s)) connection.send(new Message(MessageType.USER_ADDED, s));
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    sendBroadcastMessage(new Message(MessageType.TEXT, userName + ": " + message.getData()));
+                } else {
+                    ConsoleHelper.writeMessage("Ошибка");
+                }
+            }
+        }
+
+        public void run() {
+            ConsoleHelper.writeMessage("Установлено соединение с адресом " + socket.getRemoteSocketAddress());
+            try (Connection connection = new Connection(socket)) {
+                String userName = serverHandshake(connection);
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, userName));
+                notifyUsers(connection, userName);
+                serverMainLoop(connection, userName);
+                connectionMap.remove(userName);
+                sendBroadcastMessage(new Message(MessageType.USER_REMOVED, userName));
+            } catch (IOException | ClassNotFoundException e) {
+                ConsoleHelper.writeMessage("Ошибка соединения");
+            }
+
+            ConsoleHelper.writeMessage("Соединение закрыто");
+
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("fsdfsd");
+            }
+        }
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,77 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    protected String getServerAddress() {
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("ошибка");
+            clientConnected = false;
+        }
+    }
+
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeMessage("Ошибка");
+                socketThread.interrupt();
+            }
+            if (clientConnected) {
+                ConsoleHelper.writeMessage("Соединение установлено.\n" +
+                        "Для выхода наберите команду 'exit'.");
+            } else  {
+                ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+            }
+            while (clientConnected) {
+                String string = ConsoleHelper.readString();
+                if (string.equals("exit")) break;
+                if (shouldSendTextFromConsole()) sendTextMessage(string);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
+    public class SocketThread extends Thread {
+
+    }
+}
